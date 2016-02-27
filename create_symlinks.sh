e84d711e344ccd478b99ffcb6d06227a8500ec0a
Pierre <pierrechevaliergeol@free.fr>
2016-02-27 14:39:27 +0100

    Corrected a few things.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-FILES=*
-for f in $FILES
+cd ~/config
+shopt -s dotglob
+for f in *
 do
- ln -s $f ~
+ ln -s ~/config/$f ~
 done
+
+# remove unwanted symlinks (TODO can be vastly improved)
+rm ~/.git
+rm ~/README.md
+rm ~/create_symlinks.sh
+
