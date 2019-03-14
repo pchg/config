b3d1fad26af8e4b0e6fe1ed9fbc62c13cedab8f1
Pierre <pierrechevaliergeol@gmail.com>
2019-03-14 14:21:37 +0100

    Mise au point en conditions réelles sur un Pi dans /etc/skel/

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-echo "Creation of symlinks in upper directory .. (`..`), pointing to current directory . (`pwd`)
-shopt -s dotglob
-for f in *
+echo "Creation of symlinks in upper directory .. , pointing to various configuration files in 'config' current directory . (`pwd`)"
+cd ..
+#shopt -s dotglob
+for f in `ls -a config/`
 do
- if [ -a ../$f ]      # si f existe
+ echo "$f"
+ if [ -a $f ]      # si f existe
   then
-     mv ../$f ../$f.old    # on le renomme .old
-  fi
- ln -s ../config/$f ..        # et sinon, on fait un lien symbolique
+   mv $f $f.old    # on le renomme .old
+ fi
+ ln -s config/$f .        # et sinon, on fait un lien symbolique
 done
 
+#   echo "coucou"
 # remove unwanted symlinks (TODO can be vastly improved)
-rm ../.git
-rm ../README.md
-rm ../create_symlinks.sh
-rm ../tectri.ini
+rm .git
+rm README.md
+rm create_symlinks.sh
+rm tectri.ini
 
