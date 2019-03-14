d55d1430ff55a9f065db9e356291d919e8365b7d
Pierre <pierrechevaliergeol@gmail.com>
2019-03-14 13:41:19 +0100

    Peaufinages de configuration, comme d'hab.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,20 +3,17 @@
 shopt -s dotglob
 pwd
 for f in *
-#cd ~
 do
  if [ -a ../$f ]      # si f existe
   then
-#   if [ $f != config ]
-#    then
-     mv ../$f ../$f.old    # on le renomme .old (à moins que ce ne soit le répertoire config)
-#   fi
+     mv ../$f ../$f.old    # on le renomme .old
   fi
- ln -s config/$f ..        # et sinon, on fait un lien symbolique
+ ln -s ../config/$f ..        # et sinon, on fait un lien symbolique
 done
 
 # remove unwanted symlinks (TODO can be vastly improved)
 rm ../.git
 rm ../README.md
 rm ../create_symlinks.sh
+rm ../tectri.ini
 
