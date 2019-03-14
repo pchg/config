d2bca28cbdde6d15fc697f81fa992e2586d3b5aa
Pierre <pierrechevaliergeol@gmail.com>
2019-03-14 12:17:07 +0100

    Peaufinages de configuration, comme d'hab.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 shopt -s dotglob
-cd ..
 pwd
-for f in config/*
+for f in *
 #cd ~
 do
- if [ -a $f ]      # si f existe
+ if [ -a ../$f ]      # si f existe
   then
-   if [ $f != config ]
-    then
-     mv $f $f.old    # on le renomme .old (à moins que ce ne soit le répertoire config)
-   fi
+#   if [ $f != config ]
+#    then
+     mv ../$f ../$f.old    # on le renomme .old (à moins que ce ne soit le répertoire config)
+#   fi
   fi
- ln -s $f .        # et sinon, on fait un lien symbolique
+ ln -s $f ..        # et sinon, on fait un lien symbolique
 done
 
 # remove unwanted symlinks (TODO can be vastly improved)
-rm .git
-rm README.md
-rm create_symlinks.sh
+rm ../.git
+rm ../README.md
+rm ../create_symlinks.sh
 
